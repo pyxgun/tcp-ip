365aa1b031cfeeb4ed4d5eec3f027c92e221c198
SeijiMatsuoka <jixeis9541@gmail.com>
2022-11-02 02:22:55 +0900

    tcp data receive and connection close

--- include/packet.h
+++ include/packet.h
@@ -1,12 +1,25 @@
 #ifndef _PACKET_FUNC
 #define _PACKET_FUNC 1
 
+#include "socket_struct.h"
+#include "ip_header.h"
+#include "tcp_header.h"
+
 /* computing checksum */
 unsigned short in_cksum(unsigned short *addr, int len);
+
+/* calculate total packet size */
+size_t packet_size(unsigned short protocol, char *data);
+
+/* allocate memory for packet buffer */
+char *pballoc(size_t packet_size);
 
 /* set ipv4 header */
 void set_ipv4(char *p, struct inet_addr *src, struct inet_addr *dst,
                 unsigned short protocol, size_t len);
+
+/* convert to ip header */
+struct ip_hdr *cvt2ipv4(char *p);
 
 /* set udp datagram */
 void set_udp(char *p, struct inet_addr *src, struct inet_addr *dst,
@@ -16,10 +29,13 @@ void set_udp(char *p, struct inet_addr *src, struct inet_addr *dst,
 void set_tcp(char *p, struct inet_addr *src, struct inet_addr *dst, unsigned short sport, unsigned short dport,
                 tcp_seq seq, tcp_seq ack, uint8_t flag, uint16_t win_size, uint16_t urp, char *data);
 
-/* calculate total packet size */
-size_t packet_size(unsigned short protocol, char *data);
+/* convert to tcp header */
+struct tcp_hdr *cvt2tcp(char *p);
 
-/* allocate memory for packet buffer */
-char *pballoc(size_t packet_size);
+/* retrieve tcp payload */
+char *tcp_payload(struct tcp_hdr *p);
+
+/* get tcp payload length */
+size_t tcp_pl_len(char *p);
 
 #endif
